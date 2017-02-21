5bbab09a6b0cb1e071b15aeb1cb06126747064c8
VAUst <vasily.ustyugov@gmail.com>
2017-02-21 20:27:18 +0200

    change method define ActiveCell to right version

--- src/main/java/sample/impls/Util.java
+++ src/main/java/sample/impls/Util.java
@@ -58,6 +58,38 @@ public final class Util {
 		return list;
 	}
 	
+	private static Cell getChangedCell(Scanword scanword, int row, 
+			int column,	Cell currentCell, Cell neighboureCell, String position, String direction) {
+		Cell cell = null;
+		if ( getCountNeighbourCellsOtherType(scanword, row, column, ActiveCell.class, CommentCell.class, position).size()>0
+				|| getCountNeighbourCellsOtherType(scanword, row, column, SimpleCell.class, CommentCell.class, position).size()>0) {
+			if  (neighboureCell.getClass().getName().equals(ActiveCell.class.getName())
+					|| neighboureCell.getClass().getName().equals(SimpleCell.class.getName())) {
+				System.out.println(row+" "+column);
+				cell = new ActiveCell();
+				cell.setLetter(currentCell.getLetter());
+				cell.setFirstLink(currentCell.getFirstLink());
+				cell.setSecondLink(currentCell.getSecondLink());
+				if (direction.equals("horizontal")) {
+					((ActiveCell)cell).setHDirection(true);
+					if (currentCell.getClass().getName().equals(ActiveCell.class.getName())) {
+						((ActiveCell)cell).setVDirection(((ActiveCell)currentCell).getVDirection());
+					}
+				}
+				else if (direction.equals("vertical")) {
+					((ActiveCell)cell).setVDirection(true);
+					if (currentCell.getClass().getName().equals(ActiveCell.class.getName())) {
+						((ActiveCell)cell).setHDirection(((ActiveCell)currentCell).getHDirection());
+					}
+				}
+			}
+			else {
+				cell = currentCell;
+			}
+		}
+		return cell;
+	}
+	
 	public static boolean changeSimpleCellsToActiveCells (Scanword scanword) {
 		if (getCountFreeLinksInOneTypeCells(scanword, CommentCell.class) == 0) {
 			return false;
@@ -65,64 +97,31 @@ public final class Util {
 		else {
 			for (int row = 0; row < scanword.getRow(); row++) {
 				for (int column = 0; column < scanword.getColumns();column++) {
-					Cell cell = null;
-					Cell afterCell = null;
-					if (row == 0 ) {
-						cell = scanword.getArrayElement(row, column);
-						afterCell = scanword.getArrayElement(row + 1, column);
-					}
-					if (column == 0) {
-						cell = scanword.getArrayElement(row, column);
-						afterCell = scanword.getArrayElement(row, column + 1);
-					}
-					if (row != 0 && column != 0) {
+					if (scanword.getArrayElement(row, column).getClass().getName().equals(CommentCell.class.getName())
+							|| scanword.getArrayElement(row, column).getClass().getName().equals(DisableCell.class.getName())) {
 						continue;
 					}
-					
-					if ((cell.getClass().getName().equals(ActiveCell.class.getName())
-							|| cell.getClass().getName().equals(SimpleCell.class.getName()))
-						&& (afterCell.getClass().getName().equals(ActiveCell.class.getName())
-								|| afterCell.getClass().getName().equals(SimpleCell.class.getName()))) {
-						List<Integer[]> count = null;
-						String position = "";
-						if (column == 0 && row == 0) {
-							position = "leftTop";
+					if (row == 0) {
+						if (column == 0 ) {
+							scanword.setArrayElement(getChangedCell(scanword, row, column, scanword.getArrayElement(row, column), scanword.getArrayElement(row+1, column), "leftTop", "vertical"), row, column);
+							scanword.setArrayElement(getChangedCell(scanword, row, column, scanword.getArrayElement(row, column), scanword.getArrayElement(row, column+1), "leftTop", "horizontal"), row, column);
 						}
-						else if (column == scanword.getColumns()-1 && row == 0) {
-							position = "rightTop";
+						else if (column == scanword.getColumns()-1 ) {
+							scanword.setArrayElement(getChangedCell(scanword, row, column, scanword.getArrayElement(row, column), scanword.getArrayElement(row+1, column), "rightTop", "vertical"), row, column);
 						}
-						else if (row == 0 && column < scanword.getColumns()-1 && column > 0){
-							position = "top";
+						else {
+							scanword.setArrayElement(getChangedCell(scanword, row, column, scanword.getArrayElement(row, column), scanword.getArrayElement(row+1, column), "top", "vertical"), row, column);
 						}
-						else if (row == scanword.getRow()-1 && column == 0) {
-							position = "leftBottom";
-						}
-						else if (column == 0 && row > 0 && row < scanword.getRow()-1) {
-							position = "left";
-						}
-						count = getCountNeighbourCellsOtherType(scanword, row, column, cell.getClass(), CommentCell.class, position); 
-						if (count.size() > 0) {
-							Cell newCell = new ActiveCell();
-							newCell.setLetter(cell.getLetter());
-							newCell.setFirstLink(cell.getFirstLink());
-							newCell.setSecondLink(cell.getSecondLink());
-							if (cell.getClass().getName().equals(ActiveCell.class.getName())) {
-								((ActiveCell)newCell).setHDirection(((ActiveCell)cell).getHDirection());
-								((ActiveCell)newCell).setVDirection(((ActiveCell)cell).getVDirection());
-							}
-							if (row == 0) {
-								((ActiveCell)newCell).setHDirection(true);
-								newCell.setFirstLink("0.0");
-							}
-							if (column == 0) {
-								((ActiveCell)newCell).setVDirection(true);
-								newCell.setSecondLink("0.0");
-							}
-							scanword.setArrayElement(newCell, row, column);
-						}
+					}
+					else if (row == scanword.getRow()-1 && column == 0) {
+						scanword.setArrayElement(getChangedCell(scanword, row, column, scanword.getArrayElement(row, column), scanword.getArrayElement(row, column+1), "leftBottom", "horizontal"), row, column);
+					}
+					else if (row > 0 && row < scanword.getRow()-1 && column == 0) {
+						scanword.setArrayElement(getChangedCell(scanword, row, column, scanword.getArrayElement(row, column), scanword.getArrayElement(row, column+1), "left", "horizontal"), row, column);
 					}
 				}
 			}
+					
 			
 			for (int row = 1; row < scanword.getRow()-1;row++) {
 				for (int column = 0; column < scanword.getColumns(); column++) {
@@ -162,12 +161,25 @@ public final class Util {
 						cell.setLetter(centralCell.getLetter());
 						cell.setFirstLink(centralCell.getFirstLink());
 						cell.setSecondLink(centralCell.getSecondLink());
-						((ActiveCell)cell).setVDirection(true);
+						((ActiveCell)cell).setHDirection(true);
 						if (centralCell.getClass().getName().equals(ActiveCell.class.getName())) {
-							((ActiveCell)cell).setHDirection(((ActiveCell)centralCell).getHDirection());
+							((ActiveCell)cell).setVDirection(((ActiveCell)centralCell).getVDirection());
 						}
 						cell.setSecondLink("0.0");
 						scanword.setArrayElement(cell, row, column);
+					}
+				}
+			}
+			for (int row = 0; row < scanword.getRow(); row++) {
+				for (int column = 0; column < scanword.getColumns(); column++) {
+					Cell cell = scanword.getArrayElement(row, column);
+					if (cell instanceof ActiveCell) {
+						if (!((ActiveCell)cell).getHDirection()) {
+							cell.setFirstLink("9.9");
+						}
+						if (!((ActiveCell)cell).getVDirection()) {
+							cell.setSecondLink("9.9");
+						}
 					}
 				}
 			}
@@ -338,13 +350,7 @@ public final class Util {
 				else {
 					position = "central";
 				}
-				if (cell instanceof ActiveCell & cell.getCountFreeLink()>0) {
-					if (!((ActiveCell)cell).getHDirection() & cell.getSecondLink().equals("0.0")) {
-						cell.setSecondLink("9.9");
-					}
-					if (!((ActiveCell)cell).getVDirection() & cell.getFirstLink().equals("0.0")) {
-						cell.setFirstLink("9.9");
-					}
+				if (cell instanceof ActiveCell & cell.getCountFreeLink()==1) {
 					List<Integer[]> cells = getCountNeighbourCellsOtherType(scanword, row, column, ActiveCell.class, CommentCell.class, position);
 					if (cells.size()>0) {
 						boolean lable = true;
